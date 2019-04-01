48fe6f64a20d7f48abac16b88d02fecfe710afd2
Paweł Niziołek <pawel.r.niziolek@gmail.com>
2019-04-01 20:42:56 +0200

    Updates

---
+++ precache-manifest.2cf1b1e692c23185b87ca09c56913d49.js
@@ -0,0 +1,98 @@
+self.__precacheManifest = [
+  {
+    "revision": "dda21378b13337604781",
+    "url": "/website-portfolio/static/css/main.259a895f.chunk.css"
+  },
+  {
+    "revision": "dda21378b13337604781",
+    "url": "/website-portfolio/static/js/main.88e3c520.chunk.js"
+  },
+  {
+    "revision": "c84d5f51ad173ed9c982",
+    "url": "/website-portfolio/static/js/runtime~main.2451c7bc.js"
+  },
+  {
+    "revision": "0fc64e574ce66119804c",
+    "url": "/website-portfolio/static/js/2.fbd19b1c.chunk.js"
+  },
+  {
+    "revision": "b94469aae72e1de6ddc71637e7c622c0",
+    "url": "/website-portfolio/static/media/about-left.b94469aa.png"
+  },
+  {
+    "revision": "d3c3b85ca0046e84f08febe2b4c977e6",
+    "url": "/website-portfolio/static/media/about-left1.d3c3b85c.png"
+  },
+  {
+    "revision": "5ea564e179361390d99646674e449f9d",
+    "url": "/website-portfolio/static/media/about-left2.5ea564e1.png"
+  },
+  {
+    "revision": "f919a07dfcf55f7e0ee3ff36e4adccd4",
+    "url": "/website-portfolio/static/media/about-left3.f919a07d.png"
+  },
+  {
+    "revision": "7f28313f25d4703abbb3637ac4aa61b5",
+    "url": "/website-portfolio/static/media/about-left4.7f28313f.png"
+  },
+  {
+    "revision": "f88e71e5822ede42a74d731cf587083f",
+    "url": "/website-portfolio/static/media/about-right.f88e71e5.png"
+  },
+  {
+    "revision": "eae5b113042776df4f4f28e0bf4983ad",
+    "url": "/website-portfolio/static/media/about-right1.eae5b113.png"
+  },
+  {
+    "revision": "432612dc96e1fbfa0173312414f2bb33",
+    "url": "/website-portfolio/static/media/about-right2.432612dc.png"
+  },
+  {
+    "revision": "c7ca14d18d2ab001557f7e5f6349d224",
+    "url": "/website-portfolio/static/media/about-right3.c7ca14d1.png"
+  },
+  {
+    "revision": "1b597cc9bf8b80706d10f845fb6d088c",
+    "url": "/website-portfolio/static/media/about-right4.1b597cc9.png"
+  },
+  {
+    "revision": "8cfc5f6f0149f0aaf2577ab84cb9b0ed",
+    "url": "/website-portfolio/static/media/surykatki.8cfc5f6f.mp4"
+  },
+  {
+    "revision": "f00e9fa7e1a8e08f38c44fd31a5cb0d6",
+    "url": "/website-portfolio/static/media/slotmachine.f00e9fa7.png"
+  },
+  {
+    "revision": "0e538b0c258aed65669b33ab66f8a09e",
+    "url": "/website-portfolio/static/media/memory.0e538b0c.png"
+  },
+  {
+    "revision": "8683cd716b182981c043190df36f5272",
+    "url": "/website-portfolio/static/media/pogoda.8683cd71.png"
+  },
+  {
+    "revision": "673eb08215e2cb8f59016ecb36bec463",
+    "url": "/website-portfolio/static/media/beauty.673eb082.png"
+  },
+  {
+    "revision": "c81828c8ce40f753cd5d0ce2714b56a0",
+    "url": "/website-portfolio/static/media/gasstation.c81828c8.png"
+  },
+  {
+    "revision": "b413c16320cae90aa105078ff8294d9b",
+    "url": "/website-portfolio/static/media/to-do.b413c163.png"
+  },
+  {
+    "revision": "f6d23f2076faccffab7e2483a618cc97",
+    "url": "/website-portfolio/static/media/gimp.f6d23f20.png"
+  },
+  {
+    "revision": "a7284869ebf85d2083e253ef5c1e832b",
+    "url": "/website-portfolio/static/media/mongoDB.a7284869.jpg"
+  },
+  {
+    "revision": "8879427206622ea8c567b0a761d1de99",
+    "url": "/website-portfolio/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/website-portfolio/precache-manifest.2cf1b1e692c23185b87ca09c56913d49.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/website-portfolio/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+});
