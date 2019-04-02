1aaad9ccbed84950f24fa021770cfe4d23ee050b
Paweł Niziołek <pawel.r.niziolek@gmail.com>
2019-04-02 11:42:55 +0200

    test

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./styles/index.css";
-import App from "./layouts/App";
+import App from "layouts/App";
 
 ReactDOM.render(<App />, document.getElementById("root"));
