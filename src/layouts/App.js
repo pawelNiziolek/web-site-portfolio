918d41e15ef64899eabebbb935d53e7610bbc44f
Paweł Niziołek <pawel.r.niziolek@gmail.com>
2019-04-02 12:29:28 +0200

    install hash-source

--- src/layouts/App.js
+++ src/layouts/App.js
@@ -2,19 +2,26 @@ import React, { Component } from "react";
 import Nav from "../layouts/Nav";
 import Page from "../layouts/Page";
 import Footer from "../layouts/Footer";
-import { BrowserRouter as Router } from "react-router-dom";
+// import { BrowserRouter as Router } from "react-router-dom";
+import { createHistory, LocationProvider, Router } from "@reach/router";
+import createHashSource from "hash-source";
 import "../styles/App.css";
+
+let source = createHashSource();
+let history = createHistory(source);
 
 class App extends Component {
   render() {
     return (
-      <Router>
-        <div className="wrap-all">
-          <header>{<Nav />}</header>
-          <main>{<Page />}</main>
-          <footer>{<Footer />}</footer>
-        </div>
-      </Router>
+      <LocationProvider history={history}>
+        <Router>
+          <div className="wrap-all">
+            <header>{<Nav />}</header>
+            <main>{<Page />}</main>
+            <footer>{<Footer />}</footer>
+          </div>
+        </Router>
+      </LocationProvider>
     );
   }
 }
