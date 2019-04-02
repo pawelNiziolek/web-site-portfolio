eb9bd440d8f23ba2b338ebba01253f37a9fb4bd3
Paweł Niziołek <pawel.r.niziolek@gmail.com>
2019-04-02 11:22:01 +0200

    text

--- src/layouts/Page.js
+++ src/layouts/Page.js
@@ -1,9 +1,9 @@
 import React from "react";
 import { Route, Switch } from "react-router-dom";
-import Projects from "../pages/Projects";
-import About from "../pages/About";
-import Contact from "../pages/Contact";
-import Start from "../pages/Start";
+import Projects from "./pages/Projects";
+import About from "./pages/About";
+import Contact from "./pages/Contact";
+import Start from "./pages/Start";
 import ErrorPages from "../pages/ErrorPages";
 
 const Page = () => {
