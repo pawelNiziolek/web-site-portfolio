a02d367165ac298d5d752b155da152abb03e0ad2
Paweł Niziołek <pawel.r.niziolek@gmail.com>
2019-04-02 12:36:08 +0200

    test

--- src/layouts/App.js
+++ src/layouts/App.js
@@ -2,26 +2,19 @@ import React, { Component } from "react";
 import Nav from "../layouts/Nav";
 import Page from "../layouts/Page";
 import Footer from "../layouts/Footer";
-// import { BrowserRouter as Router } from "react-router-dom";
-import { createHistory, LocationProvider, Router } from "@reach/router";
-import createHashSource from "hash-source";
+import { BrowserRouter as Router } from "react-router-dom";
 import "../styles/App.css";
-
-let source = createHashSource();
-let history = createHistory(source);
 
 class App extends Component {
   render() {
     return (
-      <LocationProvider history={history}>
-        <Router>
-          <div className="wrap-all">
-            <header>{<Nav />}</header>
-            <main>{<Page />}</main>
-            <footer>{<Footer />}</footer>
-          </div>
-        </Router>
-      </LocationProvider>
+      <Router>
+        <div className="wrap-all">
+          <header>{<Nav />}</header>
+          <main>{<Page />}</main>
+          <footer>{<Footer />}</footer>
+        </div>
+      </Router>
     );
   }
 }
