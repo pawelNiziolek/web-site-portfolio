f83aee86186cc4d6b94f24f674ad83d347043a57
Paweł Niziołek <pawel.r.niziolek@gmail.com>
2019-04-02 15:41:50 +0200

    add HashRouter

--- src/layouts/App.js
+++ src/layouts/App.js
@@ -2,19 +2,20 @@ import React, { Component } from "react";
 import Nav from "../layouts/Nav";
 import Page from "../layouts/Page";
 import Footer from "../layouts/Footer";
-import { BrowserRouter as Router } from "react-router-dom";
+// import { BrowserRouter as Router } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import "../styles/App.css";
 
 class App extends Component {
   render() {
     return (
-      <Router>
+      <HashRouter>
         <div className="wrap-all">
           <header>{<Nav />}</header>
           <main>{<Page />}</main>
           <footer>{<Footer />}</footer>
         </div>
-      </Router>
+      </HashRouter>
     );
   }
 }
